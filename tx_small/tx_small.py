5150308e584c35cc54daa71e964839cd5317e9e5
dercodekoenig <Root@t1v-n-461e8353-w-0.europe-west4-a.c.cloudproject-351316.internal>
2022-05-30 13:07:31 +0000

    .

--- tx_small/tx_small.py
+++ tx_small/tx_small.py
@@ -22,14 +22,14 @@ warmup_steps = 5000
 batch_size = 128
 gamma = 0.99
 memory_size = 3000000
-lr  = 0.0025
-seq_len = 580
+lr  = 0.005
+seq_len = 550
 
 soft_reward_inc = 1.1
 comission = 20/100000
 
-resume = True
-#resume = False
+#resume = True
+resume = False
 
 def sample_to_x(sample):
         
